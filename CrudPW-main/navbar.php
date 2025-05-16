ddc5c1b26af5c7bcb644ae1a77ded5bc8fe79374
MaDev08 <mateusudo32@gmail.com>
2025-05-16 20:41:10 -0300

    Update navbar.php
    Mudança do título da navbar para "Projeto CRUD - ETEC"

--- CrudPW-main/navbar.php
+++ CrudPW-main/navbar.php
@@ -1,6 +1,7 @@
 
+
 <nav class="navbar navbar-dark bg-dark">
 	<div class="container-md">
-		<a class="navbar-brand" href="#">CRUD - Canal TI</a>
+		<a class="navbar-brand" href="#">Projeto CRUD - Etec</a>
 	</div>
 </nav>
